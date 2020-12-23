aad82a3be686e0e7f7c9609f2956b4da64105a34
HuangYiwei <huanghyw@gmail.com>
2020-12-23 16:37:44 +0800

    变量名错误

--- timer.py
+++ timer.py
@@ -12,7 +12,7 @@ class Timer(object):
     def __init__(self, sleep_interval=0.5):
         # '2018-09-28 22:45:50.000'
         self.buy_time = datetime.strptime(global_config.getRaw('config','buy_time'), "%Y-%m-%d %H:%M:%S.%f")
-        self.buy_time_nano = int(time.mktime(self.buy_time.timetuple()) * 1000.0 + self.buy_time.microsecond / 1000)
+        self.buy_time_ms = int(time.mktime(self.buy_time.timetuple()) * 1000.0 + self.buy_time.microsecond / 1000)
         self.sleep_interval = sleep_interval
 
         self.diff_time = self.local_jd_time_diff()
@@ -46,7 +46,7 @@ class Timer(object):
         while True:
             # 本地时间减去与京东的时间差，能够将时间误差提升到0.1秒附近
             # 具体精度依赖获取京东服务器时间的网络时间损耗
-            if self.local_time() - self.diff_time >= self.buy_time_nano:
+            if self.local_time() - self.diff_time >= self.buy_time_ms:
                 logger.info('时间到达，开始执行……')
                 break
             else:
