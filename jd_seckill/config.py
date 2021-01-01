f430c498190fe747fa557c104bdc8660ef03c7fd
HuangYiwei <huanghyw@gmail.com>
2021-01-01 14:08:31 +0800

    config.ini文件支持环境变量配置

--- jd_seckill/config.py
+++ jd_seckill/config.py
@@ -5,14 +5,25 @@ import os
 import configparser
 
 
+class EnvInterpolation(configparser.BasicInterpolation):
+    """
+    尝试从系统环境变量中读取，如果读取失败，则直接使用配置值
+    """
+
+    def before_get(self, parser, section, option, value, defaults):
+        value = super().before_get(parser, section, option, value, defaults)
+        # 无法获取则返回原值
+        return os.path.expandvars(value)
+
+
 class Config(object):
     def __init__(self, config_file='config.ini'):
         self._path = os.path.join(os.getcwd(), config_file)
         if not os.path.exists(self._path):
             raise FileNotFoundError("No such file: config.ini")
-        self._config = configparser.ConfigParser()
+        self._config = configparser.ConfigParser(interpolation=EnvInterpolation())
         self._config.read(self._path, encoding='utf-8-sig')
-        self._configRaw = configparser.RawConfigParser()
+        self._configRaw = configparser.RawConfigParser(interpolation=EnvInterpolation())
         self._configRaw.read(self._path, encoding='utf-8-sig')
 
     def get(self, section, name):
